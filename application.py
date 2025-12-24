69e2c970bbcd57107b937331d12f8d73c3907c59
Nirvighna Saxena <nirvighnasaxena7661@gmail.com>
2025-12-24 17:02:45 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,48 @@
+import pickle
+from flask import Flask , request , jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+'''
+It creates an instance of the flask class,
+which will be your WSGI application.
+'''
+
+application=Flask(__name__)
+app=application
+
+##import ridge regressor and standard scalar pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scalar=pickle.load(open('models/scaler.pkl' , 'rb'))
+
+print(type(ridge_model))
+print(type(standard_scalar))
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/predictdata" , methods=['GET' , 'POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+
+        data=standard_scalar.transform([[Temperature,RH,Ws,Rain ,FFMC , DMC , ISI , Classes , Region]])
+        result=ridge_model.predict(data)
+
+        return render_template('home.html' , results=result[0])
+    else:
+        return render_template("home.html")
+if __name__=='__main__':
+    app.run(host="0.0.0.0")
